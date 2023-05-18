0fcbd02291bc44968235288a42c56cd25b845b89
nirjhor saha <nirjhor.badhan25@gmail.com>
2023-05-18 16:54:38 +0600

    added dynamic title

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import useTitle from '../../hooks/useTitle';
 
 const SignIn = () => {
+    useTitle('Login')
     return (
         <div>
             <h1>signin page</h1>
