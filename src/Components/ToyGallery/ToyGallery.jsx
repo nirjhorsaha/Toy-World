a1fe5fd7b16062e6f0038339d10ad43d588df5bf
nirjhor saha <nirjhor.badhan25@gmail.com>
2023-05-18 17:21:37 +0600

    added toys gallery section on home page

---
+++ src/Components/ToyGallery/ToyGallery.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const ToyGallery = () => {
+    return (
+        <div>
+            <h1 className='text-center text-5xl uppercase text-orange-400 font-bold'>Toys Gallery</h1>
+            <div class="grid md:grid-cols-4 gap-4 mt-6">
+                <div>
+                    <img src="https://img.freepik.com/free-photo/toy-truck-with-christmas-trees_1252-293.jpg?w=740&t=st=1684407821~exp=1684408421~hmac=b71b9af1a44cc8661d99ebf46aaefbbd711d5805a5f10ab4ddcda907377ab46b" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/young-boy-playing-indoors-with-eco-toys_23-2150268109.jpg?w=740&t=st=1684408025~exp=1684408625~hmac=996397e2be62f0231f49c0956508de5e1d2174de69184848a56460b4f53f9f19" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/cute-kid-playing-with-wooden-train-side-view_23-2149357243.jpg?w=740&t=st=1684408045~exp=1684408645~hmac=08c3e66b7d9f8dd82deec42ffb1e74b058aa693668665b89b43968f6b6aa46d8" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/close-up-toy-car-with-easter-eggs_23-2149318378.jpg?w=740&t=st=1684408075~exp=1684408675~hmac=a6a7bb751d26b4e4ccb235f44644600ca644b66269c96feb238354d017a3dbda" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/green-easter-car-with-copy-space_23-2149301292.jpg?w=740&t=st=1684408183~exp=1684408783~hmac=ff7411d4f7f9d717441750152b5b68820a54e5b32f3744763c68fcea331dc0d1" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/business-planning-concept-side-view-forklift-truck-stacking-wooden-blocks_176474-9256.jpg?w=740&t=st=1684408117~exp=1684408717~hmac=957c31ecf6b5920bc062d7021bcad36f8a5a3e908ba2d9918023ec39c270e0c3" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/red-pickup-model-black-floor_1150-16350.jpg?w=740&t=st=1684408105~exp=1684408705~hmac=92d4b7c6fa764c5d1c30bed98489e81fa9dfb74b7e1878d2a682d12e11284c37" className='rounded-lg' alt="" srcset="" />
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/closeup-old-mini-police-car-toy_181624-45993.jpg?w=740&t=st=1684408162~exp=1684408762~hmac=9948a66d6b3c409dffd83c6f32611dfac6bbfd66eb014c299aa00cc8331a39a9" className='rounded-lg' alt="" srcset="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ToyGallery;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
 import Header from '../../Components/Header/Header';
+import ToyGallery from '../../Components/ToyGallery/ToyGallery';
 
 const Home = () => {
     useTitle('Home');
     return (
         <div>
-            <Header/>
+            <Header />
+            <ToyGallery/>
         </div>
     );
 };
